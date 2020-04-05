281cc519fd40181e4afbf36b38c8758436447e68
Sommie09 <ceekay2a7@gmail.com>
2020-04-05 20:16:51 +0100

    Initial commit

---
+++ src/main/java/com/packtpub/http/HeadRequest.java
@@ -0,0 +1,42 @@
+package com.packtpub.http;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+public class HeadRequest {
+    public static void main(String[] args) {
+        String path = "https://www.packtpub.com/";//Domain for practicing
+
+        try {
+            URL url = new URL(path);//Object for path to perform HTTP functions
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();//Open Connection
+
+            connection.setRequestMethod("HEAD");//Set HEAD
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Code: " + responseCode);
+
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                System.out.println("Got an unexpected response code");
+            }
+
+            System.out.println( connection.getResponseMessage() );
+
+            // Returned HTTP headers.
+            Map<String, List<String>> headers = connection.getHeaderFields();
+
+            for (String key : headers.keySet()) {
+                System.out.println(key + ": " + headers.get(key));
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
